b522008d1074a19e928ebb4daccddf68b0f0a099
ねず <reborn.dance1985@gmail.com>
2023-09-14 05:52:25 +0900

    has_many, belongs_to を記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -22,6 +22,8 @@ class Article < ApplicationRecord
   validates :content, length: { minimum: 10 }
   validates :content, uniqueness: true
 
+  belongs_to :user
+
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
