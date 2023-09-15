07c5bdd46ecaf312d82ba40ca42c502897994cca
ねず <reborn.dance1985@gmail.com>
2023-09-15 16:44:38 +0900

    ユーザ名を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -26,5 +28,9 @@ class User < ApplicationRecord
 
   def has_written?(article)
     articles.exists?(id: article.id)
+  end
+  
+  def display_name
+    self.email.split('@').first
   end
 end
